9e5fd72eb8e5445ae2afd6e434b5d128e8727600
kaynaremre <emrekaynar_8@hotmail.com>
2020-10-15 12:12:12 +0300

    a

--- week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -4,8 +4,14 @@
 
 using std::vector;
 
+using namespace std;
+
+
 long long max_dot_product(vector<int> a, vector<int> b) {
-  // write your code here
+
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+
   long long result = 0;
   for (size_t i = 0; i < a.size(); i++) {
     result += ((long long) a[i]) * b[i];
