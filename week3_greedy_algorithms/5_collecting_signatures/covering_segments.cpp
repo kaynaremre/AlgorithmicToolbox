2cdf66671bc57247c0277b729495f5f27b72e4f6
kaynaremre <emrekaynar_8@hotmail.com>
2020-10-21 12:25:58 +0300

    a

--- week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
+++ week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
@@ -5,13 +5,27 @@
 
 using std::vector;
 
+using namespace std; 
+
 struct Segment {
   int start, end;
 };
 
 vector<int> optimal_points(vector<Segment> &segments) {
   vector<int> points;
-  //write your code here
+  Segment tmp;
+  vector<long long> endpoints;
+
+  for (int j = 0; j < segments.size(); ++j){
+    segments.at(j) = tmp;
+    endpoints.at(j) = tmp.end;
+  }
+
+  sort(endpoints.begin(), endpoints.end());
+
+  
+
+
   for (size_t i = 0; i < segments.size(); ++i) {
     points.push_back(segments[i].start);
     points.push_back(segments[i].end);
